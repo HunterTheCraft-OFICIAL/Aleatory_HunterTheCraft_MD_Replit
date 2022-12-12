66813af4e6424f9ce4628e48e5a83efd221bb3a3
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2022-12-12 03:37:50 -0300

    first commit

--- dono/menus/infodono.js
+++ dono/menus/infodono.js
@@ -4,20 +4,26 @@ const infodono = (prefix, numerodn, NomeDoBot, sender) => {
 //${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.
 
 return`
-╭───────────────┐
-├ INFORMAÇÕES DO DONO
-├───────────────
-│ Solicitou o CMD: @${sender.split("@")[0]}
-├───────────────┐
-│ Dono: wa.me/${numerodn} 
-╞───────────────┘
-╰──────────┐
-╭──────────┴─┐
-│ INFORMAÇÕES DO BOT
-├────────────
-│Prefixo: ⩺ ${prefix}
-│Nome: ⩺ ${NomeDoBot} 
-╰─────────────╯
+★ ▬▬▬▬▬🫡▬▬▬▬▬ ★
+INFORMAÇÕES DO DONO
+
+• Usuário que Solicitou o 
+Comando:
+ @${sender.split("@")[0]}
+
+★ ▬▬▬▬▬🫡▬▬▬▬▬ ★
+*•〢 DONO 🧑‍💻*
+• wa.me/${numerodn}
+
+★ ▬▬▬▬▬🫡▬▬▬▬▬ ★
+*•〢 INFORMAÇÕES DO BOT*
+
+• Prefixo: ${prefix} 
+
+• Nome:
+${NomeDoBot} 
+
+★ ▬▬▬▬▬🫡▬▬▬▬▬ ★
 `
 }
 
